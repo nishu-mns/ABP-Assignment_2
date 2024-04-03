b70dff99b499bab8e02065234b5a612188a8151d
nishu-mns <nisha.makwana2423@gmail.com>
2024-04-03 20:34:22 +0530

    initial commit

---
+++ aspnet-core/src/PasswordlessAuthentication.EntityFrameworkCore/EntityFrameworkCore/PasswordlessAuthenticationDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace PasswordlessAuthentication.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class PasswordlessAuthenticationDbContextFactory : IDesignTimeDbContextFactory<PasswordlessAuthenticationDbContext>
+{
+    public PasswordlessAuthenticationDbContext CreateDbContext(string[] args)
+    {
+        // https://www.npgsql.org/efcore/release-notes/6.0.html#opting-out-of-the-new-timestamp-mapping-logic
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        PasswordlessAuthenticationEfCoreEntityExtensionMappings.Configure();
+
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<PasswordlessAuthenticationDbContext>()
+            .UseNpgsql(configuration.GetConnectionString("Default"));
+
+        return new PasswordlessAuthenticationDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../PasswordlessAuthentication.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
